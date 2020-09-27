db277fb02ca51699aecf84133dcf82ddb158e78a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-27 19:41:01 +0530

    Updates

---
+++ precache-manifest.4d1c9616b00391815ad0f3e946a9df18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ca373a7aba780276e5c718aa04cb67",
+    "url": "/react-task-list/index.html"
+  },
+  {
+    "revision": "247cd07f39d7f3c38411",
+    "url": "/react-task-list/static/css/main.c5298b20.chunk.css"
+  },
+  {
+    "revision": "d135d1658838fcb04033",
+    "url": "/react-task-list/static/js/2.01e34748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task-list/static/js/2.01e34748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247cd07f39d7f3c38411",
+    "url": "/react-task-list/static/js/main.52dbf5e5.chunk.js"
+  },
+  {
+    "revision": "5893737a459fcf2801ac",
+    "url": "/react-task-list/static/js/runtime-main.4c1cd64c.js"
+  }
+]);
